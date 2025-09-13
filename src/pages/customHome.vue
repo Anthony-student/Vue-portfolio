591879f28eefcb20fcc61382a796a616c139a315
Anthony-AIR-03 <108930332+Anthony-AIR-03@users.noreply.github.com>
2025-09-13 17:32:13 +0200

    implemented snowfall

--- src/pages/customHome.vue
+++ src/pages/customHome.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import HomeThreeBody from '@/components/pages/home-three/HomeThreeBody.vue';
+import SnowFall from '@/components/shared/SnowFall.vue';
 
 </script>
 
 <template>
     <HomeThreeBody />
-
+    <SnowFall />
 </template>
