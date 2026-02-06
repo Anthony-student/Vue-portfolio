5d0c9214f622179a0af0d3a0faa57ad65445a5a9
Anthony-AIR-03 <108930332+Anthony-AIR-03@users.noreply.github.com>
2026-02-06 14:00:15 +0100

    Added experiences to personalData

--- src/assets/data/personalData.ts
+++ src/assets/data/personalData.ts
@@ -3,5 +3,27 @@ export const personalData = {
     socials: {
         linkedIn: "https://www.linkedin.com/in/anthony-inoc%C3%AAncio-ramos-b89003277/",
         instagram: "https://www.instagram.com/anthony.air/"
-    }
+    },
+    experiences: [
+        {
+            name: "YEARS EXPERIENCE",
+            count: "6",
+            prefix: "0",
+        },
+        {
+            name: "TOTAL PROJECTS",
+            count: "21",
+            prefix: "+",
+        },
+        {
+            name: "CLIENTS WORLDWIDE",
+            count: "6",
+            prefix: "+",
+        },
+        // {
+        //     name: "TOTAL COMMITS",
+        //     count: "115",
+        //     prefix: "+",
+        // }
+    ]
 }
